7d0fb758e8f02e3d00b8472729959c3ba82ed1c9
Milo van Emaus <38001005+milodepilo@users.noreply.github.com>
2022-12-07 18:52:23 +0100

    changed to .sh for script

--- .github/scripts/script.sh
+++ .github/scripts/script.sh
@@ -1,7 +1,4 @@
-cd farmblog
-echo ${{secrets.PASSWORD }} | sudo -S git switch blog       
-echo ${{secrets.PASSWORD }} | sudo -S git pull git@github.com:milodepilo/farm.git blog
-echo ${{secrets.PASSWORD }} | sudo -S python3 -m venv venv
+
 source venv/bin/activate
 pip install farmblog-1.0.0-py3-none-any.whl
 export FLASK_APP=farmblog
