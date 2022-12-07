01aa0840a9c7759193453d9e153f928851d9a023
Milo van Emaus <38001005+milodepilo@users.noreply.github.com>
2022-12-07 19:53:41 +0100

    changed to .sh for script

--- .github/scripts/script.sh
+++ .github/scripts/script.sh
@@ -1,5 +1,5 @@
 
-python3 -m venv venv
-source venv/bin/activate
+python3 -m venv ~/farmflask/farmblog/venv
+source ~/farmflask/farmblog/venv/bin/activate
 pip install --force-reinstall farmblog-1.0.0-py3-none-any.whl
 flask --app farmblog init-db
